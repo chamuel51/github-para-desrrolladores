59abe1dd3c1a5a898e945531823550b69c20fa09
chamuel51 <chamuel.castillo@gmail.com>
2016-02-26 00:08:03 -0400

    Update holaMundo.java

--- holaMundo.java
+++ holaMundo.java
@@ -3,7 +3,9 @@ public class holaMundo{
 
 public static void main(String[] args){
 
-  System.out.println("hola Mundo!!);
+ 
+ System.out.println("hola Mundo!!);
+ System.out.println("adios mundo!");
   }
 }
 
